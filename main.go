2a44c1bfb4d7f1f6165f15e928d1b8270f22f2d3
qoxogus <a01034367055@gmail.com>
2020-11-04 11:41:10 +0900

    first echo

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+// func main() {
+// 	scrapper.Scrape("term")
+// }
+
 func main() {
-	scrapper.Scrape("term")
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323")) // localhost:1323
+
 }
