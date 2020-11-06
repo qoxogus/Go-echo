2e73909dd2e847b287b87959ab70f48a68cf1334
qoxogus <a01034367055@gmail.com>
2020-11-06 14:04:35 +0900

    login study

--- main.go
+++ main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo"
 )
 
-func handlelog(c echo.Context) error {
-	return c.File("login.html")
+func handleHello(c echo.Context) error {
+	return c.String(http.StatusOK, "공부 화이팅")
 }
 
 func main() {
-	// Echo instance
 	e := echo.New()
-
-	// Middleware
-	// e.Use(middleware.Logger())
-	// e.Use(middleware.Recover())
-
-	// Route => handler
-	e.GET("/", handlelog)
-
-	// Start server
+	e.GET("/", handleHello)
 	e.Logger.Fatal(e.Start(":1323"))
 }
